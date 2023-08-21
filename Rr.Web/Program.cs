21740bebb2a1d5d1d4e54a1470ca25b7c0aff463
Pozdrawiam <witam@tuta.io>
2023-08-21 20:14:29 +0200

    App start log

--- Rr.Web/Program.cs
+++ Rr.Web/Program.cs
@@ -1,9 +1,7 @@
-using Microsoft.EntityFrameworkCore;
 using Rr.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<IDb, Db>(options =>
@@ -11,21 +9,19 @@ builder.Services.AddDbContext<IDb, Db>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.Logger.LogInformation("Starting app at {}", DateTime.Now);
 
 app.Run();
