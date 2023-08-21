e84cda5a58969ad419d8b8c2d37dfb246f7a07f0
Pozdrawiam <witam@tuta.io>
2023-08-21 20:07:48 +0200

    Db auto create

--- Rr.Web/Pages/Index.cshtml.cs
+++ Rr.Web/Pages/Index.cshtml.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rr.Core;
 
 namespace Rr.Web.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly Db _db;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, Db db)
     {
         _logger = logger;
+        _db = db;
     }
 
-    public void OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
+        bool dbCreated = await _db.Database.EnsureCreatedAsync();
 
+        if (dbCreated)
+            _logger.LogInformation("Database created");
+
+        return RedirectToPage("./HttpMonitors/Index");
     }
 }
