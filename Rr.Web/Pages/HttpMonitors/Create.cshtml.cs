7afe284c4a1a07903be1e78493471c5968f3441d
Pozdrawiam <witam@tuta.io>
2023-08-22 21:31:20 +0200

    Basic create page

---
+++ Rr.Web/Pages/HttpMonitors/Create.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Rr.Core;
+using Rr.Core.HttpMonitors;
+
+namespace Rr.Web.Pages.HttpMonitors;
+
+public class CreateModel : PageModel
+{
+    private readonly IDb _db;
+
+    public CreateModel(IDb db)
+    {
+        _db = db;
+    }
+    
+    [BindProperty]
+    public HttpMonitor HttpMonitor { get; set; } = new();
+    
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        _db.HttpMonitors.Add(HttpMonitor);
+        await _db.SaveChangesAsync();
+        
+        return RedirectToPage("./Index");
+    }
+}
