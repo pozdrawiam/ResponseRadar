5449dc7704da0e39cbfaf0823e184b229dc93a01
Pozdrawiam <witam@tuta.io>
2023-08-26 11:12:20 +0200

    Add validation

--- Rr.Core/Data/HttpMonitor.cs
+++ Rr.Core/Data/HttpMonitor.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Rr.Core.Data;
 
 public class HttpMonitor
 {
     public int Id { get; set; }
+    
+    [MinLength(4), MaxLength(50)]
     public string Name { get; set; } = "";
+    
+    [MinLength(5), MaxLength(1000), Url]
     public string Url { get; set; } = "";
 }
