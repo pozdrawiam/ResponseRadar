509cb2d1de9a63fc25e1f9015203baad62ac4af0
Pozdrawiam <witam@tuta.io>
2023-08-29 19:30:16 +0200

    Error handling in MonitorWorker

--- Rr.Web/Services/MonitorWorker.cs
+++ Rr.Web/Services/MonitorWorker.cs
@@ -5,23 +5,33 @@ namespace Rr.Web.Services;
 public class MonitorWorker : BackgroundService
 {
     private readonly TimeSpan _interval;
+    private readonly ILogger<MonitorWorker> _logger;
     private readonly IServiceScopeFactory _scopeFactory;
 
-    public MonitorWorker(IAppConfig config, IServiceScopeFactory scopeFactory)
+    public MonitorWorker(IAppConfig config, ILogger<MonitorWorker> logger, IServiceScopeFactory scopeFactory)
     {
         _interval = TimeSpan.FromMinutes(config.IntervalMinutes);
+        _logger = logger;
         _scopeFactory = scopeFactory;
     }
-
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            using var scope = _scopeFactory.CreateScope();
-            IMonitorService monitorService = scope.ServiceProvider.GetRequiredService<IMonitorService>();
-            
-            await monitorService.CheckUrlsAsync();
-            await Task.Delay(_interval, stoppingToken);
+            try
+            {
+                using var scope = _scopeFactory.CreateScope();
+                IMonitorService monitorService = scope.ServiceProvider.GetRequiredService<IMonitorService>();
+
+                await monitorService.CheckUrlsAsync();
+                await Task.Delay(_interval, stoppingToken);
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "MonitorWorker error");
+            }
         }
     }
 }
