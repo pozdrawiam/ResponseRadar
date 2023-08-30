10f61c434a9bcce1e08ac24f0c0474f547c5bbce
Pozdrawiam <witam@tuta.io>
2023-08-30 20:45:24 +0200

    Fix validation url when contains port number

--- Rr.Core/Data/HttpMonitor.cs
+++ Rr.Core/Data/HttpMonitor.cs
@@ -2,17 +2,25 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Rr.Core.Data;
 
-public class HttpMonitor
+public class HttpMonitor : IValidatableObject
 {
     public int Id { get; set; }
     
     [MinLength(4), MaxLength(50)]
     public string Name { get; set; } = "";
     
-    [MinLength(5), MaxLength(1000), Url]
+    [MinLength(5), MaxLength(1000)]
     public string Url { get; set; } = "";
 
     public DateTime CheckedAt { get; set; }
 
     public int Status { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Url.Length > 5 && !Uri.TryCreate(Url, UriKind.Absolute, out _))
+        {
+            yield return new("Invalid URL format.", new[] { nameof(Url) });
+        }
+    }
 }
