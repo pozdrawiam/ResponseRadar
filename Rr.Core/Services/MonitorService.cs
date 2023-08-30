98ec1e1b8eb21dbc72eb2f59d4c4f1962cd59cac
Pozdrawiam <witam@tuta.io>
2023-08-30 21:52:01 +0200

    Refactor of MonitorService

--- Rr.Core/Services/MonitorService.cs
+++ Rr.Core/Services/MonitorService.cs
@@ -37,44 +37,54 @@ public class MonitorService : IMonitorService
 
         foreach (HttpMonitor monitor in monitors.Where(x => !string.IsNullOrWhiteSpace(x.Url)))
         {
-            HttpResponseMessage? response = null;
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            
-            try
-            {
-                response = await _httpService.GetAsync(monitor.Url);
-                stopwatch.Stop();
-            }
-            catch (Exception e)
-            {
-                if (e is HttpRequestException hre)
-                    _logger.LogWarning(e, "Monitor '{}' request failed", monitor.Name);
-                else
-                    _logger.LogError(e, "Monitor '{}' request failed", monitor.Name);
-                
-                await _notificationService.NotifyAsync(
-                    "Monitor '{0}' failed with {1}", monitor.Name, e.GetType().Name);
-                
-                continue;
-            }
-            finally
-            {
-                monitor.CheckedAt = DateTime.UtcNow;
-                monitor.Status = (int?)(response?.StatusCode) ?? 0;
-                monitor.ResponseTimeMs = stopwatch.ElapsedMilliseconds <= int.MaxValue ? (int)stopwatch.ElapsedMilliseconds : 0;
-                
-                _db.AttachModified(monitor);
-                await _db.SaveChangesAsync();
-            }
-            
-            if (response.StatusCode == HttpStatusCode.OK)
-                _logger.LogInformation("Monitor '{}' ok", monitor.Name);
-            else
-            {
-                _logger.LogWarning("Monitor '{}' failed with status {}", monitor.Name, response.StatusCode.ToString());
-                await _notificationService.NotifyAsync(
-                    "Monitor '{0}' failed with status {1} {2}", monitor.Name, (int)response.StatusCode, response.StatusCode.ToString());
-            }
+            await CheckUrlAsync(monitor);
         }
+    }
+
+    private async Task CheckUrlAsync(HttpMonitor monitor)
+    {
+        HttpResponseMessage? response = null;
+        Stopwatch stopwatch = Stopwatch.StartNew();
+            
+        try
+        {
+            response = await _httpService.GetAsync(monitor.Url);
+            stopwatch.Stop();
+        }
+        catch (Exception e)
+        {
+            if (e is HttpRequestException hre)
+                _logger.LogWarning(e, "Monitor '{}' request failed", monitor.Name);
+            else
+                _logger.LogError(e, "Monitor '{}' request failed", monitor.Name);
+                
+            await _notificationService.NotifyAsync(
+                "Monitor '{0}' failed with {1}", monitor.Name, e.GetType().Name);
+                
+            return;
+        }
+        finally
+        {
+            await UpdateMonitorAsync(monitor, response?.StatusCode, stopwatch.ElapsedMilliseconds);
+        }
+            
+        if (response.StatusCode == HttpStatusCode.OK)
+            _logger.LogInformation("Monitor '{}' ok", monitor.Name);
+        else
+        {
+            _logger.LogWarning("Monitor '{}' failed with status {}", monitor.Name, response.StatusCode.ToString());
+            await _notificationService.NotifyAsync(
+                "Monitor '{0}' failed with status {1} {2}", monitor.Name, (int)response.StatusCode, response.StatusCode.ToString());
+        }
+    }
+
+    private async Task UpdateMonitorAsync(HttpMonitor monitor, HttpStatusCode? status, long elapsedMs)
+    {
+        monitor.CheckedAt = DateTime.UtcNow;
+        monitor.Status = (int?)status ?? 0;
+        monitor.ResponseTimeMs = elapsedMs <= int.MaxValue ? (int)elapsedMs : 0;
+                
+        _db.AttachModified(monitor);
+        await _db.SaveChangesAsync();
     }
 }
