6cb1a352627f13417db4cc2fafb610b3bdc1cdb4
Pozdrawiam <witam@tuta.io>
2023-09-03 21:13:41 +0200

    Custom timeout field - db migration

---
+++ Rr.Core/Data/Migrations/20230903190342_TimeoutMs.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rr.Core.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeoutMs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TimeoutMs",
+                table: "HttpMonitors",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeoutMs",
+                table: "HttpMonitors");
+        }
+    }
+}
