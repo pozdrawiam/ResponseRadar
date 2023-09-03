a6976de9b06a3f4338843ab049a723623b1e676b
Pozdrawiam <witam@tuta.io>
2023-09-03 20:29:52 +0200

    Creating HttpClient per request

--- Rr.Core/Services/IHttpService.cs
+++ Rr.Core/Services/IHttpService.cs
@@ -8,6 +8,31 @@ public interface IHttpService
     Task<HttpResponseMessage> PostAsync(string url, HttpContent content);
 }
 
-public class HttpService : HttpClient, IHttpService
+public class HttpService : IHttpService
 {
+    public TimeSpan Timeout { get; set; }
+    
+    public async Task<HttpResponseMessage> GetAsync(string url)
+    {
+        using HttpClient client = GetClient();
+
+        return await client.GetAsync(url);
+    }
+
+    public async Task<HttpResponseMessage> PostAsync(string url, HttpContent content)
+    {
+        using HttpClient client = GetClient();
+
+        return await client.PostAsync(url, content);
+    }
+
+    private HttpClient GetClient()
+    {
+        var client = new HttpClient();
+        
+        if (Timeout != default)
+            client.Timeout = Timeout;
+
+        return client;
+    }
 }
